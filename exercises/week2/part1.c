4bf34505272e731043dbbfbbf50cb2b3690609ac
Aleck Zhao <aleck.zhao1114@gmail.com>
2016-09-14 15:43:14 -0400

    Adding part 1 of exercise

---
+++ exercises/week2/part1.c
@@ -0,0 +1,17 @@
+/*
+ * part1.c
+ * prints "Hellow, world, my name if Aleck"
+ *
+ * author: Aleck Zhao
+ * Date: 9/14/2016
+ */
+
+#include <stdio.h>
+
+int main() {
+	printf("Hello, world, my name is Aleck\n");
+	
+	return 0;	
+}
+
+
